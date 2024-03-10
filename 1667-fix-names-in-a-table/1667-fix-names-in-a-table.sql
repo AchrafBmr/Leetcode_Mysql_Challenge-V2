1ec27a958f3e2aada4680764fababdee2f0da74c
achrafbmr <dawddawd555@gmail.com>
2024-03-10 18:36:56 +0000

    Update 1667-fix-names-in-a-table.sql

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,7 +1,2 @@
 
-SELECT Users.user_id ,
-  CONCAT(UPPER(SUBSTR(Users.name,1,1))
-  ,LOWER(SUBSTR(Users.name,2))) AS name 
-FROM Users
-ORDER BY
-Users.user_id ASC
+SELECT Users.user_id , CONCAT(UPPER(SUBSTR(Users.name,1,1)) ,LOWER(SUBSTR(Users.name,2))) AS name FROM Users ORDER BY Users.user_id ASC
