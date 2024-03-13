a72fd97d62aed612f12cd79480f6ab6f8939ef5b
achrafbmr <dawddawd555@gmail.com>
2024-03-13 16:41:09 +0000

    Update 1484-group-sold-products-by-the-date.sql

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -4,4 +4,5 @@ select sell_date, count( DISTINCT product ) as num_sold ,
     GROUP_CONCAT( DISTINCT product order by product ASC separator ',' ) as products
     
         FROM Activities 
-    GROUP BY sell_date order by sell_date ASC;
+    GROUP BY sell_date 
+    order by sell_date ASC;
